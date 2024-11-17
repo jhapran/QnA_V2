6dc3f01d81cb0df75a8e7b0c2abcb011231e2d75
jhapran <30171129+jhapran@users.noreply.github.com>
2024-11-18 00:02:32 +0530

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { Header } from './header';
+import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 import { Footer } from './Footer';
 
